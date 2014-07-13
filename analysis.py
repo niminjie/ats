90e68103391f614f22e5def499150651eb30d2fc
niminjie <niminjiecide@gmail.com>
2014-07-13 14:12:41 +0800

    Stat all car types rank score

--- analysis.py
+++ analysis.py
@@ -13,7 +13,7 @@ class Analysis():
         '''
         self._comments = dataset.comments
 
-    def get_onelevel_comment(self, key):
+    def get_onelevel_comment(self, key, comments):
         '''
         {key1: {key2: value}}
         Get one level: {key2: value}
@@ -24,9 +24,25 @@ class Analysis():
             one_comment   Comment value
         '''
         one_comment = []
-        for c in self._comments:
+        for c in comments:
             one_comment.append(c[key.decode('gbk')])
         return one_comment
+
+    def filter_comment_list(self, constrain = None):
+        '''
+        constrain: {'crawlURL':'', 'pubtime': '', '裸车购买价':'', '购买地点':'','购买时间':'','购买车型':'','购买车系':'','购车目的':'','购车经销商':'' }
+        '''
+        if constrain == None:
+            return self._comments
+        rank = []
+        for c in self._comments:
+            match_constrain = True
+            for key, value in constrain.items():
+                if value != c[key]:
+                    match_constrain = False
+            if match_constrain:
+                rank.append(c)
+        return rank
 
     def avg_rate(self, item):
         '''
@@ -57,7 +73,7 @@ class Analysis():
     def _convert_to_float(self, l):
         return [float(i) for i in l]
 
-    def stat_rank(self, fields):
+    def stat_rank(self, fields, comment):
         '''
         Statistic all avg rate in fields
         ---------------------------------
@@ -67,21 +83,11 @@ class Analysis():
             None
         '''
         for f in fields:
-            f_value = self.get_onelevel_comment(f)
+            f_value = self.get_onelevel_comment(f, comment)
             print f.decode('gbk'), '\t', self.avg_rate(f_value)
 
-def test():
-    ATS = DataSet('./dataset/ATS.txt')
-    BMW = DataSet('./dataset/BMW.txt')
-    Benz = DataSet('./dataset/Benz.txt')
-    Audi = DataSet('./dataset/Audi.txt')
-    # ATS.json_print()
-    ana_ATS = Analysis(ATS)
-    ana_BMW = Analysis(BMW)
-    ana_Benz = Analysis(Benz)
-    ana_Audi = Analysis(Audi)
 
-
+def ana_gen_score():
     field = ['空间', '动力', '操控', '油耗', '舒适性', '外观', '内饰', '性价比']
     print 'ATS'
     ana_ATS.stat_rank(field)
@@ -95,22 +101,37 @@ def test():
     print 'Audi'
     ana_Audi.stat_rank(field)
 
-    # car = ana_Audi.get_onelevel_comment('购买车型')
-    # for c in car:
+
+def print_car_type(ana, versus = True):
+    car = ana.get_onelevel_comment('购买车型', ana.filter_comment_list())
+    # for c in set(car):
     #     print c
-    
+    return set(car)
 
-    # space = ana_ATS.get_onelevel_comment('空间')
-    # dynamic = ana_ATS.get_onelevel_comment('动力')
-    # control = ana_ATS.get_onelevel_comment('操控')
-    # fuel = ana_ATS.get_onelevel_comment('油耗')
-    # comfort = ana_ATS.get_onelevel_comment('舒适性')
-    # appearance = ana_ATS.get_onelevel_comment('外观')
-    # trim = ana_ATS.get_onelevel_comment('内饰')
-    # price = ana_ATS.get_onelevel_comment('性价比')
+def test():
+    ATS = DataSet('./dataset/ATS.txt')
+    BMW = DataSet('./dataset/BMW.txt')
+    Benz = DataSet('./dataset/Benz.txt')
+    Audi = DataSet('./dataset/Audi.txt')
+    # ATS.json_print()
+    ana_ATS = Analysis(ATS)
+    ana_BMW = Analysis(BMW)
+    ana_Benz = Analysis(Benz)
+    ana_Audi = Analysis(Audi)
 
-    # print ana_ATS.avg_rate(space)
-    # print ana_ATS.avg_rate(dynamic)
+    all_car_type = print_car_type(ana_Audi)
+    # print all_car_type
+
+    field = ['空间', '动力', '操控', '油耗', '舒适性', '外观', '内饰', '性价比']
+
+    for t in all_car_type:
+        dic = {}
+        # dic['购买车型'.decode('gbk')] = '2012款 325i 豪华型'.decode('gbk')
+        dic['购买车型'.decode('gbk')] = t
+        print t
+        ats_28t = ana_Audi.filter_comment_list(constrain=dic)
+        if ats_28t:
+            ana_Audi.stat_rank(field, ats_28t)
 
 def main():
     pass
